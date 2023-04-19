04062bcca233d4595b21639671d088192abff393
virginia-oc <94895896+virginia-oc@users.noreply.github.com>
2023-04-19 18:41:18 +0100

    Fix spinner

--- app/src/main/java/edu/virginiaojeda/cuencamovil/fragments/IncidentFragment.kt
+++ app/src/main/java/edu/virginiaojeda/cuencamovil/fragments/IncidentFragment.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import edu.virginiaojeda.cuencamovil.R
+import edu.virginiaojeda.cuencamovil.databinding.HomeFragmentBinding
 import edu.virginiaojeda.cuencamovil.databinding.IncidentFragmentBinding
 
 class IncidentFragment : Fragment() {
     lateinit var binding : IncidentFragmentBinding
+    private lateinit var contextFrag :Context
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        contextFrag = context
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,15 +31,60 @@ class IncidentFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = IncidentFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter.createFromResource(
+            contextFrag,
+            R.array.sp_categories,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
+        binding.spCategories.adapter = adapter
     }
 
     override fun onStart() {
         super.onStart()
+        binding.spCategories.setSelection(0)
+        binding.spCategories.onItemSelectedListener =
+            object : AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position > 0){
+                        Toast.makeText(
+                            contextFrag,
+                            "pues el spinner",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    binding.spCategories.setSelection(0)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onItemClick(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    TODO("Not yet implemented")
+                }
+            }
     }
 
     override fun onResume() {
